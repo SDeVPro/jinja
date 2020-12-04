4937b7279cfc8db6364aa616815ee842bb65b4bf
SDeVPro <sardor5676626@gmail.com>
2020-12-04 18:16:37 +0500

    first commit

---
+++ product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('addcomment/<int:id>', views.addcomment, name='addcomment'),
+]
